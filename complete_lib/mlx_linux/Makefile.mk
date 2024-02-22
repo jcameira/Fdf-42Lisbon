05edffa4a8e30487ac58f4abce75b499bda68e6e
João Lucas <joao_carlos1995@hotmail.com>
2024-02-22 00:59:33 +0000

    libs fixed

---
+++ complete_lib/mlx_linux/Makefile.mk
@@ -0,0 +1,97 @@
+##
+## Makefile for MiniLibX in /home/boulon/work/c/raytraceur/minilibx
+## 
+## Made by Olivier Crouzet
+## Login   <ol@epitech.net>
+## 
+## Started on  Tue Oct  5 15:56:43 2004 Olivier Crouzet
+## Last update Tue May 15 15:41:20 2007 Olivier Crouzet
+##
+
+## Please use configure script
+
+
+INC	=%%%%
+
+UNAME = $(shell uname)
+CC	= gcc
+ifeq ($(UNAME),FreeBSD)
+	CC = clang
+endif
+
+NAME		= libmlx.a
+NAME_UNAME	= libmlx_$(UNAME).a
+
+SRC	= mlx_init.c mlx_new_window.c mlx_pixel_put.c mlx_loop.c \
+	mlx_mouse_hook.c mlx_key_hook.c mlx_expose_hook.c mlx_loop_hook.c \
+	mlx_int_anti_resize_win.c mlx_int_do_nothing.c \
+	mlx_int_wait_first_expose.c mlx_int_get_visual.c \
+	mlx_flush_event.c mlx_string_put.c mlx_set_font.c \
+	mlx_new_image.c mlx_get_data_addr.c \
+	mlx_put_image_to_window.c mlx_get_color_value.c mlx_clear_window.c \
+	mlx_xpm.c mlx_int_str_to_wordtab.c mlx_destroy_window.c \
+	mlx_int_param_event.c mlx_int_set_win_event_mask.c mlx_hook.c \
+	mlx_rgb.c mlx_destroy_image.c mlx_mouse.c mlx_screen_size.c \
+	mlx_destroy_display.c
+
+OBJ_DIR = ../../objects/
+OBJ	= $(addprefix $(OBJ_DIR),$(SRC:%.c=%.o))
+CFLAGS	= -O3 -I$(INC)
+
+TOTAL_SRCS          =   $(words $(SRC))
+FILES				=	0
+
+all	: $(NAME)
+
+$(OBJ_DIR)%.o: %.c
+	@mkdir -p $(OBJ_DIR)
+	@$(CC) $(CFLAGS) $(IFLAGS) -c $< -o $@ 2>/dev/null
+	$(eval FILES=$(shell echo $$(($(FILES) + 1)))) \
+	$(call PRINT_PROGRESS,$(TOTAL_SRCS),$(GRN),$(YELLOW)Compiling$(DEFAULT) $@)
+
+$(NAME)	: $(OBJ)
+	ar -r $(NAME) $(OBJ)
+	ranlib $(NAME)
+	cp $(NAME) $(NAME_UNAME)
+	@echo "\033[2F\033[0K$(CYAN)$(NAME_UNAME)$(DEFAULT) successfully created\033[E"
+
+check: all
+	@test/run_tests.sh
+
+show:
+	@printf "NAME  		: $(NAME)\n"
+	@printf "NAME_UNAME	: $(NAME_UNAME)\n"
+	@printf "CC		: $(CC)\n"
+	@printf "CFLAGS		: $(CFLAGS)\n"
+	@printf "SRC		:\n	$(SRC)\n"
+	@printf "OBJ		:\n	$(OBJ)\n"
+
+clean	:
+	@rm -rf $(OBJ_DIR) $(NAME) $(NAME_UNAME) *~ core *.core
+
+.PHONY: all check show clean
+
+define PRINT_PROGRESS
+    if [ "$(FILES)" -eq "1" ]; then \
+        printf "\033[0K$(3)\n["; \
+    else \
+        printf "\033[0K\033[1F\033[0K$(3)\n["; \
+    fi
+    @for i in `seq 1 $(shell expr $(FILES) \* 70 / $(1))`; do \
+        printf "$(2)=\033[0m"; \
+    done
+    @for i in `seq 1 $(shell expr 70 - $(FILES) \* 70 / $(1))`; do \
+        printf " "; \
+    done
+    @printf "] $(shell echo $$(($(FILES) * 100 / $(1))))%%"
+	if [ "$(FILES)" -eq "$(1)" ]; then \
+        printf "\n"; \
+	fi
+endef
+
+CYAN				=	\033[36m
+PURPLE				=	\033[35m
+YELLOW				=	\033[33m
+GRN					=	\033[32m
+RED					=	\033[31m
+DEFAULT				=	\033[0m
